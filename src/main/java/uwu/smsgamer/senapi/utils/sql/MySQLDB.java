e78fb7c0b42ff00f0f53e947e01bbdf9fe33fe8d
true-cc <creepercannyon@gmail.com>
2020-12-09 15:30:19 -0800

    Almost done MySQLDB I think.

--- src/main/java/uwu/smsgamer/senapi/utils/sql/MySQLDB.java
+++ src/main/java/uwu/smsgamer/senapi/utils/sql/MySQLDB.java
@@ -5,6 +5,9 @@ import uwu.smsgamer.senapi.utils.Pair;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * An Implementation of {@link SenDB} for MySQL.
+ */
 public class MySQLDB implements SenDB {
     public String host;
     public int port;
@@ -58,19 +61,36 @@ public class MySQLDB implements SenDB {
 
     @Override
     public Table getTable(String table) {
-        return null;
+        return new MySQLTable(this, table);
     }
 
     @Override
-    public Table createTable(String name, String contents) {
+    public Table createTable(String name, String columns) {
         try {
             PreparedStatement ps = getConnection().prepareStatement(
-              "CREATE TABLE IF NOT EXISTS " + tablePrefix + name + " (" + contents + ")");
+              "CREATE TABLE IF NOT EXISTS " + tablePrefix + name + " (" + columns + ")");
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return new MySQLTable(this, name);
+    }
+
+    @Override
+    public boolean tableExists(String name) {
+        try {
+            Connection connection = getConnection();
+            if (connection == null) return false;
+
+            DatabaseMetaData metadata = connection.getMetaData();
+            if (metadata == null) return false;
+
+            ResultSet rs = metadata.getTables(null, null, name, null);
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     private static class MySQLTable implements SenDB.Table {
@@ -82,24 +102,110 @@ public class MySQLDB implements SenDB {
             this.name = name;
         }
 
-
         @Override
-        public void addToRow(String row, Object value) {
-
+        public void delete() {
+            try {
+                db.update("DROP TABLE " + name + ";");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
-        public List<String> getRows() {
+        public void truncate() {
+            try {
+                db.update("TRUNCATE TABLE " + name + ";");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public int countRows() {
+            try {
+                ResultSet rs = db.query("SELECT * FROM " + name);
+                int i = 0;
+                while (rs.next()) i++;
+                return i;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }
+
+        @Override
+        public boolean exists(String column, String values) {
+            try {
+                ResultSet rs = db.query("SELECT * FROM " + name + " WHERE " + column + "=" + values + ";");
+                return rs.next();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        public void addToTable(String columns, String values) {
+            try {
+                db.update("INSERT INTO " + name + " (" + columns + ") VALUES (" + values + ");");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void removeFromTable(String columns, String operand, String checkData) {
+            try {
+                db.update("DELETE FROM " + name + " WHERE " + columns + operand + checkData + ";");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void upsert(String selected, Object object, String column, String checkData) {
+            // TODO: 2020-12-09
+        }
+
+        @Override
+        public void set(String selected, Object object, String[] where) {
+            if (where.length == 0) return;
+            if (object != null) object = "'" + object + "'";
+            String condition = Table.getCondition(where);
+            try {
+                db.update("UPDATE " + name + " SET " + selected + "=" + object + " WHERE " + condition + ";");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public Object get(String selected, String[] where) {
+            if (where.length == 0) return null;
+            String condition = Table.getCondition(where);
+            try {
+                ResultSet rs = db.query("SELECT * FROM " + name + " WHERE " + condition + ";");
+                if (rs.next()) {
+                    return rs.getObject(selected);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             return null;
         }
 
         @Override
-        public List<Object> getRow(String row) {
-            return null;
-        }
-
-        @Override
-        public Map<String, List<Object>> getMultipleRows(String... rows) {
+        public List<Object> getList(String selected, String[] where) {
+            if (where.length == 0) return null;
+            String condition = Table.getCondition(where);
+            try {
+                ResultSet rs = db.query("SELECT * FROM " + name + " WHERE " + condition + ";");
+                List<Object> list = new ArrayList<>();
+                if (rs.next()) list.add(rs.getObject(selected));
+                return list;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             return null;
         }
     }
--- src/main/java/uwu/smsgamer/senapi/utils/sql/SenDB.java
+++ src/main/java/uwu/smsgamer/senapi/utils/sql/SenDB.java
@@ -3,7 +3,7 @@ package uwu.smsgamer.senapi.utils.sql;
 import uwu.smsgamer.senapi.utils.Pair;
 
 import java.sql.*;
-import java.util.*;
+import java.util.List;
 
 public interface SenDB {
     void initialize(Pair<String, String>... rows);
@@ -30,23 +30,62 @@ public interface SenDB {
 
     Table getTable(String table);
 
-    Table createTable(String name, String contents);
+    Table createTable(String name, String columns);
+
+    boolean tableExists(String name);
+
+    default void update(String update) throws SQLException {
+        PreparedStatement ps = getConnection().prepareStatement(update);
+        ps.executeUpdate();
+    }
+
+    default ResultSet query(String query) throws SQLException {
+        PreparedStatement ps = getConnection().prepareStatement(query);
+        return ps.executeQuery();
+    }
+
 
     interface Table {
-        void addToRow(String row, Object value);
+        void delete();
 
-        List<String> getRows();
+        void truncate(); // Clears
 
-        List<Object> getRow(String row);
+        int countRows();
 
-        default Map<String, List<Object>> getAllRow() {
-            return getMultipleRows(getRows().toArray(new String[0]));
+        boolean exists(String column, String values);
+
+        void addToTable(String columns, String values);
+
+        void removeFromTable(String columns, String operand, String checkData);
+
+        void upsert(String selected, Object object, String column, String checkData);
+
+        default void set(String selected, Object object, String column, String operand, String checkData) {
+            set(selected, object, new String[]{column + operand + checkData});
         }
 
-        Map<String, List<Object>> getMultipleRows(String... rows);
+        void set(String selected, Object object, String[] where);
 
-        default int getRowLength(String row) {
-            return getRow(row).size();
+        default Object get(String selected, String column, String operand, String checkData) {
+            return get(selected, new String[]{column + operand + checkData});
+        }
+
+        Object get(String selected, String[] where);
+
+        default List<Object> getList(String selected, String column, String operand, String checkData){
+            return getList(selected, new String[]{column + operand + checkData});
+        }
+
+        List<Object> getList(String selected, String[] where);
+
+        static String getCondition(String[] where) {
+            StringBuilder condition = new StringBuilder();
+            for (int i = 0; i < where.length - 1; i++) {
+                String s = where[i];
+                condition.append(s).append(" AND ");
+            }
+            condition.append(where[where.length - 1]);
+            return condition.toString();
         }
     }
 }
